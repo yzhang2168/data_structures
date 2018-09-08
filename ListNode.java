bd3159d33ed19932d12066a6dc58cd8b813ae821
Yun Zhang <33489605+yzhang2168@users.noreply.github.com>
2018-09-08 13:09:38 -0700

    Add files via upload

--- ListNode.java
+++ ListNode.java
@@ -2,7 +2,7 @@ package data_structures;
 
 public class ListNode {
     int      value;
-    public ListNode next; // a reference to ListNode object
+    ListNode next; // a reference to ListNode object
     
     public ListNode(int x) {
     	value = x;
@@ -13,6 +13,46 @@ public class ListNode {
     	value = x;
     	next = n;
     }
+    
+    
+    /**
+     * Generate a linked list of length n where the nodes contains numbers from 0 to n-1 in order.
+     * Assumption: n > 0.
+     * Example: n = 3
+     * Answer: 0->1->2->null
+     * */    
+    public static ListNode generateLinkedList(int n) {
+    	ListNode head = new ListNode(0);
+    	ListNode current = head;
+    	for (int i = 1; i < n; i++) {
+    		current.next = new ListNode(i);
+    		current = current.next;
+    	}
+    	return head;
+    }
+
+    
+    /**
+     * Given a linked list, count the number of nodes in it.
+     * Assumption: The linkedlist is not null or empty.
+     * Example:
+     * Given: 1->7->5->4->2->null
+     * Answer: 5
+     * */
+    public static int countNodes(ListNode head) {
+        int count = 0;
+        ListNode current = head;
+        // corner case
+        if (head == null) {
+        	return 0;
+        }
+        while (current != null) {
+        	count++;
+        	current = current.next;
+        }        	
+        return count;
+    }
+    
     
     /**
      * Reverse a singly-linked list.
@@ -65,6 +105,7 @@ public class ListNode {
     	*/
     }
     
+    
     // recursion
     public static ListNode reverseRecursion(ListNode head) {
     	/**
@@ -92,3 +133,176 @@ public class ListNode {
     	head.next = null;
     	return newHead;
     }
+    
+    
+    /**
+     * find the middle node of a linked list
+     * in case of even number of nodes, return the 1st of the 2 middle nodes 
+     * */ 
+    public static ListNode findMiddle(ListNode head) {
+    	// corner case
+    	if (head == null) {
+    		return head;
+    	}
+    	ListNode s = head;
+    	ListNode f = head;
+    	// return the 2nd of the 2 middle nodes in case of even number of nodes 
+    	//while(f != null && f.next != null) {
+    	
+    	// return the 1st of the 2 middle nodes in case of even number of nodes
+    	while(f.next != null && f.next.next != null) {
+    		s = s.next;
+    		f = f.next.next;
+    	}
+    	return s;
+    	}
+    
+    
+    /**
+     * detect cycle in a singly linked list
+     * Floyd's tortoise and hare algorithm
+     * */ 
+    public static boolean detectCycle(ListNode head) {
+    	ListNode slow = head;
+    	ListNode fast = head;
+    	while(fast != null && fast.next != null) {
+    		slow = slow.next;
+    		fast = fast.next.next;
+    		if (slow == fast) {
+    			return true;
+    		}
+    	}
+    	// if hare reaches end of list, no loop found
+    	return false;
+    }
+    
+    
+    /**
+     * insert a node into a sorted linked list
+     * */ 
+    public static ListNode insertNode(ListNode head, int target) {
+		ListNode newNode = new ListNode(target);
+    	// corner case || inserting before the head
+    	if (head == null || head.value >= target) {
+    		newNode.next = head;
+    		return newNode;
+    	}
+    	// target > head: inserting in the middle
+    	// in case of identical values, insert before identical
+    	ListNode curr = head;
+    	while (curr.next != null) {
+    		if (curr.value < target && curr.next.value >= target) {
+    			newNode.next = curr.next;
+    			curr.next = newNode;
+    			return head;
+    		}
+    		curr = curr.next;
+    	}
+    	// after while(), curr points to tail
+    	// target > tail: inserting after the tail
+		curr.next = newNode;
+    	return head;
+    }
+
+    
+    public static ListNode insertNode2(ListNode head, int target) {
+  		ListNode newNode = new ListNode(target);
+      	// corner case || inserting before the head
+      	if (head == null || head.value >= target) {
+      		newNode.next = head;
+      		return newNode;
+      	}
+      	// target > head: inserting in the middle
+      	// in case of identical values, insert before identical
+      	ListNode curr = head;
+      	ListNode prev = null;
+      	while (curr != null) {
+      		if (curr.value < target) { 
+      			prev = curr;
+      			curr = curr.next;
+      		} else { // if (curr.value >= target)
+      			newNode.next = curr;
+      			prev.next = newNode;
+      			return head;
+      		}
+      	}
+      	// after while(), curr points to null
+      	// target > tail: inserting after the tail
+  		prev.next = newNode;
+      	return head;
+      }
+    
+    
+    /**
+     * merge 2 sorted linked list into one sorted list
+     * L1 = 1 -> 4 -> 6 -> null, L2 = 2 -> 5 -> null, merge L1 and L2 to 1 -> 2 -> 4 -> 5 -> 6 -> null
+     * L1 = null, L2 = 1 -> 2 -> null, merge L1 and L2 to 1 -> 2 -> null
+     * L1 = null, L2 = null, merge L1 and L2 to null
+     * */ 
+    public ListNode merge(ListNode one, ListNode two) {
+    	/**
+    	 * without using a dummy node, need to check corner cases
+   	
+    	ListNode head = null;
+    	if (one == null) {
+    		return two;
+    	} 
+    	if (two == null) {
+    		return one;
+    	}
+    	if (one.value < two.value) {
+    		head = one;
+    		one = one.next;
+    	} else {
+    		head = two;
+    		two = two.next;
+    	}
+    	
+    	ListNode curr = head;
+    	
+   	 * */
+     	// use a dummy node makes code a lot simpler
+    	// also covers corner cases automatically (list 1 is null, list 2 is null, or both null)
+    	ListNode curr1 = one;
+    	ListNode curr2 = two;
+    	ListNode dummy = new ListNode(-1);
+    	ListNode curr = dummy;
+    	while(curr1 != null && curr2 != null) {
+    		if (curr1.value <= curr2.value) {
+    			curr.next = curr1;
+    			curr1 = curr1.next;
+    		} else {
+    			curr.next = curr2;
+    			curr2 = curr2.next;
+    		}
+			curr = curr.next;
+    	}
+    	// if curr1 has remaining nodes
+    	if (curr1 != null) {
+    		curr.next = curr1;
+    	}
+    	// if curr2 has remaining nodes
+    	if (curr2 != null) {
+    		curr.next = curr2;
+    	}
+    	// exclude dummy node
+    	return dummy.next;
+    }
+    
+    /**
+     * Given a linked list and a target value T, partition it such that 
+     * all nodes less than T are listed before the nodes larger than or equal to target value T. 
+     * The original relative order of the nodes in each of the two partitions should be preserved.
+     * L = 2 -> 4 -> 3 -> 5 -> 1 -> null, T = 3, 
+     * is partitioned to 2 -> 1 -> 4 -> 3 -> 5 -> null
+     * */
+    public static ListNode partition(ListNode head, int target) {
+    	// corner case
+    	if (head == null) {
+    		return head;
+    	}
+    	
+    	return head;
+    }
+
+}
